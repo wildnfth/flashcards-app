9f397d23b2251224da30c51838676893a725bf57
Wildan Faqihul Futuh <wildnfth@gmail.com>
2024-03-13 18:22:03 +0700

    nambahin fetch ke js

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+fetch('kotoba.json')
+    .then(response => response.json())
+    .then(data => {
+            const cards = document.querySelectorAll('.card');
+
+            // Display data in HTML elements
+        cards.forEach((card, index) => {
+            card.querySelector('.kanji').textContent = data[index].kanji;
+            card.querySelector('.kana').textContent = data[index].kana;
+            card.querySelector('.example-jp').textContent = data[index]['example-jp'];
+        })
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
