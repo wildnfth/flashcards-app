0be9c0439cdea2f3407fb8000f65c45527992519
Wildan Faqihul Futuh <wildnfth@gmail.com>
2024-03-22 20:22:39 +0700

    phase 1 tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */module.exports = {
+/** @type {import('tailwindcss').Config} */ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
